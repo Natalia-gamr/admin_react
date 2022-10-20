aa4843128630e593dbdf0bbc0bb1195d9e36b92b
Natalie <fogel666@yandex.ru>
2022-10-20 17:13:57 +0300

    editing links

--- app/src/components/editor/editor-text/editor-text.js
+++ app/src/components/editor/editor-text/editor-text.js
@@ -2,12 +2,25 @@ export default class EditorText {
     constructor(element, virtualElement) {
         this.element = element;
         this.virtualElement = virtualElement;
+        this.element.addEventListener("click", () => this.onClick());
+        this.element.addEventListener("blur", () => this.onBlur());
+        this.element.addEventListener("keypress", (e) => this.onKeypress(e));
+        this.element.addEventListener("input", () => this.onTextEdit());
+        if (this.element.parentNode.nodeName === "A" || this.element.parentNode.nodeName === "BUTTON") {
+            this.element.addEventListener("contextmenu", (e) => this.onCtxMenu(e));
+        }
+    }
 
-        this.element.addEventListener('click', () => this.onClick())
-        this.element.addEventListener('blur', () => this.onBlur())
-        this.element.addEventListener('keypress', (e) => this.onKeypress(e))
-        this.element.addEventListener('input', () => this.onTextEdit())
-    } 
+    onCtxMenu(e) {
+        e.preventDefault();
+        this.onClick();
+    }
+
+    onKeypress(e) {
+        if(e.keyCode === 13) {
+            this.element.blur();
+        }
+    }
 
     onClick() {
         this.element.contentEditable = "true";
@@ -15,13 +28,7 @@ export default class EditorText {
     }
 
     onBlur() {
-        this.element.removeAttribute('contenteditable')
-    }
-
-    onKeypress(e) {
-        if(e.keyCode === 13 ) {
-            this.element.blur();
-        }
+        this.element.removeAttribute('contenteditable');
     }
 
     onTextEdit() {
